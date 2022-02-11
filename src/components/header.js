f540ff13a329548aee60f29e7a171c4201d3314f
Benjamin F <84413548+bforrest-pct@users.noreply.github.com>
2022-02-11 05:01:22 -0800

    task1

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,26 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+  
+  const headerDiv = document.createElement('div')
+  headerDiv.classList.add('header')
+
+  const dateSpan = document.createElement('span')
+  dateSpan.classList.add('date')
+  dateSpan.textContent = date
+
+  const titleH1 = document.createElement('h1')
+  titleH1.textContent = title
+
+  const tempSpan = document.createElement('span')
+  tempSpan.classList.add('temp')
+  tempSpan.textContent = temp
+
+  headerDiv.appendChild(dateSpan)
+  headerDiv.appendChild(titleH1)
+  headerDiv.appendChild(tempSpan)
+
+  return headerDiv
 }
 
 const headerAppender = (selector) => {
